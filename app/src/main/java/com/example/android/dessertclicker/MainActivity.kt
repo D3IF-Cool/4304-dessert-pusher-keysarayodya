0124657eb9738abb449e9d67a8ef1151e3b533b8
keysarayodya <keysarayodya@gmail.com>
2021-03-24 13:12:10 +0700

    Challenge

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -29,6 +29,7 @@ import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
 const val KEY_REVENUE = "revenue_key"
 const val KEY_DESSERT_SOLD = "dessert_sold_key"
+const val KEY_TIMER = "key_timer"
 
 class MainActivity : AppCompatActivity() {
 
@@ -81,6 +82,7 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState != null) {
             revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
             dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            dessertTimer.secondsCount = savedInstanceState.getInt(KEY_TIMER, 0)
             showCurrentDessert()
         }
         // Set the TextViews to the right values
@@ -125,6 +127,7 @@ class MainActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
         outState.putInt(KEY_REVENUE, revenue)
         outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+        outState.putInt(KEY_TIMER, dessertTimer.secondsCount)
     }
 
     /**
